381c4a76f17e3d3b5572f8d32fe6c3480df46ca9
Ranjit Poudel <ranjitsparrow2@gmail.com>
2025-11-09 17:58:04 +0545

    Adding cart features with jquery and javascript

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -14,3 +14,15 @@ class Cart():
         
         # Make sure cart is available to every part of the project
         self.cart = cart
+        
+    def add(self, product):
+      product_id = str(product.id)
+    #logic
+      if product_id in self.cart:
+        pass
+      else:
+          self.cart[product_id] = {'price': str(product.price)}
+      self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart)
--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
 def cart_detail(request):
     return render(request, 'cart_detail.html', {})
 
 def cart_add(request):
-    pass
+    #Get the cart
+    cart = Cart(request)
+    #test for the post
+    if request.POST.get('action') == 'post':
+        #get stuff
+        product_id = int(request.POST.get('product_id'))
+
+        #Lookup product in DB
+        product = get_object_or_404(Product, id = product_id)
+
+        #Save to session
+        cart.add(product = product)
+
+        #Get Carat Quantity
+        cart_quantity = cart.__len__()
+        #Return Response
+        # response = JsonResponse({'Product name:' :product.name})
+        response = JsonResponse({'qty' :cart_quantity})
+        return response
+
+
+
+
+
 def cart_delete(request):
     pass
 def cart_update(request):
