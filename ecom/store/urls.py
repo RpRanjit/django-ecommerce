001a88e7161ff17ae38ec71fb8f4908001e4fdd1
Ranjit Poudel <ranjitsparrow2@gmail.com>
2025-11-16 17:33:43 +0545

    Debug the register error and add user info

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
    path('register/', views.register, name = 'register'),
    path('user_update/', views.user_update, name = 'user_update'),
    path('update_password/', views.update_password, name = 'update_password'),
+   path('update_info/', views.update_info, name = 'update_info'),
    path('logout/', views.logout_view, name = 'logout'),
    path('product/<int:pk>/', views.product, name='product'),
    path('category/<str:comm>', views.category, name = 'category'),
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render, redirect
-from .models import Product, Category
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
-# from django.contrib.auth.forms import UserCreationForm
-# from django.forms import forms
-from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 
 
 # Create your views here.
+
+def update_info(request):
+	if request.user.is_authenticated:
+		# Get Current User
+		current_user = Profile.objects.get(user__id=request.user.id)
+		# Get original User Form
+		form = UserInfoForm(request.POST or None, instance= current_user)
+		if form.is_valid():
+			# Save original form
+			form.save()
+			messages.success(request, "Your Info Has Been Updated!!")
+			return redirect('home')
+		return render(request, "update_info.html", {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In To Access That Page!!")
+		return redirect('home')
+
 def update_password(request):
     if request.user.is_authenticated:
         current_user = request.user
@@ -97,19 +113,20 @@ def logout_view(request):
       return redirect('home')
 
 def register(request):
-    form = SignUpForm()
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username= username,password = password )
-            login(request, user)
-            messages.success(request,'You are registered to Newa Ecommerce.')
-            return redirect('home')
-        else:
-            messages.success(request,'Oops! Something is wrong. Please register again.')
-            return redirect('register')
-    else:
-        return render(request, 'register.html',{'form': form})
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			# log in user
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("Username Created - Please Fill Out Your User Info Below..."))
+			return redirect('update_info')
+		else:
+			messages.success(request, ("Whoops! There was a problem Registering, please try again..."))
+			return redirect('register')
+	else:
+          form = SignUpForm()
+          return render(request, 'register.html',{'form':form})
