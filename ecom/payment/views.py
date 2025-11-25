0e778a930b9428f95dd4247135f498896ebe4954
Ranjit Poudel <ranjitsparrow2@gmail.com>
2025-11-25 19:54:38 +0545

    Order Checkout

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-
+from cart.cart import Cart
+from payment.forms import Shippingform
+from payment.models import ShippingAddress
 # Create your views here.
 def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
+
+
+def checkout(request):
+    #Get cart
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    quantities= cart.get_quants()
+    totals = cart.cart_totals
+
+    if request.user.is_authenticated:
+        # Checkout as Login user
+        #shipping user
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        # shipping form
+        shipping_form = Shippingform(request.POST or None, instance=shipping_user)
+        return render(request, 'payment/checkout.html', {'cart_products' :cart_products, 'quantities': quantities, 'totals': totals, 'shipping_form': shipping_form})
+    else:
+        # Checkout as Guest
+        shipping_form = Shippingform(request.POST or None)
+        return render(request, 'payment/checkout.html', {'cart_products' :cart_products, 'quantities': quantities, 'totals': totals,'shipping_form': shipping_form})
